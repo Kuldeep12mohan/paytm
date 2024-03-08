a114766a67824b02a80c48ad0a07b2b19e5b89ac
kuldeepkumar <mohankuldeep1234@gmail.com>
2024-03-08 14:09:13 +0530

    backend completed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const zod = require("zod");
-const { User } = require("../db");
+const { User, Account } = require("../db");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config");
 const { authMiddleware } = require("../middleware");
@@ -39,14 +39,17 @@ router.post("/signup", async (req, res) => {
     lastName: req.body.lastName,
   });
   const userId = user._id;
-
+  await Account.create({
+    userId,
+    balance:1 + Math.random()*10000
+  })
   const token = jwt.sign(
     {
       userId,
     },
     JWT_SECRET
   );
-
+ 
   res.json({
     message: "User created successfully",
     token: token,
@@ -107,5 +110,34 @@ router.put("/", authMiddleware, async (req, res) => {
     message: "Updated successfully",
   });
 });
+router.get("/bulk",async (req,res)=>
+{
+  const filter = req.query.filter ||"";
+
+  const users = await User.find({
+    $or:[
+      {
+        firstName:{
+          "$regex":filter
+        },
+      }
+      ,
+      {
+        lastName:{
+          "$regex":filter
+        }
+      }
+    ]
+  })
+  res.json({
+    user:users.map(user =>({
+      username:user.username,
+      firstName:user.firstName,
+      lastName:user.lastName,
+      _id:user._id,
+    }
+    ))
+  })
+})
 
 module.exports = router;
